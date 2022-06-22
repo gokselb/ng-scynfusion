b83a8f085c2471aaf6e8cc195680c906ac65cfe5
Burak Goksel <burak.goksel@prospeum.de>
2022-06-22 15:07:54 +0200

    initiate firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,82 @@
-import { Component } from '@angular/core';
 
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+
+import {
+  RichTextEditorComponent,
+  ToolbarService,
+  PasteCleanupSettingsModel,
+  LinkService,
+  ImageService,
+} from '@syncfusion/ej2-angular-richtexteditor';
+import { PasteCleanupService } from '@syncfusion/ej2-angular-richtexteditor';
+import {
+  HtmlEditorService,
+  CountService,
+  QuickToolbarService,
+} from '@syncfusion/ej2-angular-richtexteditor';
+import {
+  Link,
+  Count,
+  HtmlEditor,
+  QuickToolbar,
+} from '@syncfusion/ej2-angular-richtexteditor';
+import { TextBox } from '@syncfusion/ej2-inputs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    ToolbarService,
+    LinkService,
+    ImageService,
+    HtmlEditorService,
+    CountService,
+    QuickToolbarService,
+    PasteCleanupService,
+  ]
 })
 export class AppComponent {
   title = 'ng-snycfusion';
+  @ViewChild('pasteCleanupRTE')
+  public rteObj: RichTextEditorComponent;
+
+  @ViewChild('formatOption')
+  public formatObj: any;
+
+  @ViewChild('prompt')
+  public promptObj: any;
+
+  @ViewChild('plainText')
+  public plainTextObj: any;
+
+  @ViewChild('keepFormat')
+  public keepFormatObj: any;
+
+  @ViewChild('cleanFormat')
+  public cleanFormatObj: any;
+
+  @ViewChild('deniedTags')
+  public deniedTags: TextBox;
+
+  @ViewChild('deniedAttributes')
+  public deniedAttributes: TextBox;
+
+  @ViewChild('allowedStyleProperties')
+  public allowedStyleProperties: TextBox;
+
+  public pasteCleanupSettings: PasteCleanupSettingsModel = {
+    prompt: true,
+    plainText: false,
+    keepFormat: false,
+  };
+
+  public formatData: { [key: string]: Object }[] = [
+    { Id: 'prompt', Format: 'Prompt' },
+    { Id: 'plainTextFormatting', Format: 'Plain Text' },
+    { Id: 'keepFormating', Format: 'Keep Format' },
+    { Id: 'Clean Formatting', Format: 'Clean Format' },
+  ];
+  public fields: any = { text: 'Format', value: 'Format' };
+  public height: string = '200px';
+  public value: string = 'Prompt';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { RichTextEditorModule } from '@syncfusion/ej2-angular-richtexteditor';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RichTextEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
